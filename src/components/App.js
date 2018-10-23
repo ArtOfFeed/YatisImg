a54c5bd217e36acd6dbdfa097a2737b1754423c6
Vovk Eugene <zekavovk7@gmail.com>
2018-10-23 23:57:49 +0300

    step 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const CLOUDINARY_UPLOAD_PRESET = 'hfcizvyu';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/react-cloudinary/upload';
+
 class App extends Component {
+    state = {
+        uploadedFile: null,
+        uploadedFileCloudinaryUrl: ''
+    };
     selectFileHandler = (event) => {
-        console.log(event.target)
+        console.log(event.target.files[0])
     };
     submitFileHandler = () => {
-
+        console.log('1')
     };
     render() {
         return (
             <div className="App">
                 <input type="file" id='img_load' onChange={this.selectFileHandler}/>
                 <label htmlFor="img_load">Pick your image</label>
-                <button>Upload Image</button>
+                <button onSubmit={this.submitFileHandler}>Upload Image</button>
             </div>
         );
     }
