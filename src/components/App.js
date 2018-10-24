b33bb6f7cc1c38710873a48f2292597dd8e66b63
Vovk Eugene <zekavovk7@gmail.com>
2018-10-25 01:13:17 +0300

    step 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import axios from 'axios';
+import {Image} from 'cloudinary-react';
 
-const CLOUDINARY_UPLOAD_PRESET = 'hfcizvyu';
-const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/ucg/image/upload';
-const CLOUDINARY_API_KEY = '332768736618512';
+const CLOUDINARY_UPLOAD_PRESET = 'bs28jua5';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/ucg/upload';
 
 class App extends Component {
     state = {
         uploadedFile: null,
-        uploadedFileCloudinaryUrl: ''
+        uploadedFileCloudinaryUrl: '',
+        gallery: []
     };
     selectFileHandler = event => {
         this.setState({
@@ -19,23 +19,49 @@ class App extends Component {
     submitFileHandler = () => {
         console.log(this.state.uploadedFile);
         const fd = new FormData();
-        fd.append('api_key', CLOUDINARY_API_KEY);
-        fd.append('image', this.state.uploadedFile, this.state.uploadedFile.name);
-        fd.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
-        axios.post(CLOUDINARY_UPLOAD_URL)
-            .then((res) => {
-                console.log(res);
+        fd.append('file', this.state.uploadedFile, this.state.uploadedFile.name);
+        fd.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+        for (let pair of fd.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]);
+        }
+        const config = {
+            method: "POST",
+            body: fd
+        };
+        return fetch(CLOUDINARY_UPLOAD_URL, config)
+            .then(res => {
+                return res.json();
             })
-            .catch((err)=> {
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    uploadedFileCloudinaryUrl: data.secure_url
+                });
+            })
+            .catch(err => {
                 console.error(err);
-            });
+            })
     };
+    componentDidMount() {
+        const config = {
+            method: "GET"
+        };
+        fetch('https://api.cloudinary.com/v1_1/ucg/resources/image/upload', config)
+            .then(res => {
+                console.log(res.data.resources);
+                this.setState({gallery: res.data.resources});
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
     render() {
         return (
             <div className="App">
                 <input type="file" id='img_load' onChange={this.selectFileHandler}/>
                 <label htmlFor="img_load">Pick your image</label>
                 <button onClick={this.submitFileHandler}>Upload Image</button>
+                <Image cloudName="demo" publicId={this.state.uploadedFileCloudinaryUrl} width="300" crop="scale"/>
             </div>
         );
     }
